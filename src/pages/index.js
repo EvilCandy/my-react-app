fbce1c01185c0c296bf05983648823f3a9464134
EvilCandy <carman_ho_1@hotmail.com>
2020-05-18 15:31:53 +1000

    Watch video button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React apps</h1>
     <p>Complete courses about best tools and design systems. Prototype and build apps with React and Swift.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch video</Link>
   </div>
 )
 
